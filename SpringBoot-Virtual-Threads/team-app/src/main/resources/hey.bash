a4441cef696031dca957f417df7d9db8be05756f
Shrikant Kale <kaleshrikant@live.com>
2025-07-20 00:21:42 +0530

    benchmaring post Virtual-Threads

--- SpringBoot-Virtual-Threads/team-app/src/main/resources/hey.bash
+++ SpringBoot-Virtual-Threads/team-app/src/main/resources/hey.bash
@@ -42,3 +42,48 @@ Details (average, fastest, slowest):
 Status code distribution:
   [200]	32 responses
 
+## Post enabling Virtual Thread --> benchmark
+
+$ hey -n 32 -c 32 http://localhost:8080/
+
+  Summary:
+    Total:	2.4099 secs
+    Slowest:	2.4094 secs
+    Fastest:	2.4041 secs
+    Average:	2.4060 secs
+    Requests/sec:	13.2788
+
+
+  Response time histogram:
+    2.404 [1]	|■■■■
+    2.405 [9]	|■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+    2.405 [6]	|■■■■■■■■■■■■■■■■■■■■■■
+    2.406 [1]	|■■■■
+    2.406 [1]	|■■■■
+    2.407 [0]	|
+    2.407 [0]	|
+    2.408 [11]	|■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+    2.408 [1]	|■■■■
+    2.409 [1]	|■■■■
+    2.409 [1]	|■■■■
+
+
+  Latency distribution:
+    10% in 2.4043 secs
+    25% in 2.4045 secs
+    50% in 2.4055 secs
+    75% in 2.4076 secs
+    90% in 2.4079 secs
+    95% in 2.4094 secs
+    0% in 0.0000 secs
+
+  Details (average, fastest, slowest):
+    DNS+dialup:	0.0009 secs, 2.4041 secs, 2.4094 secs
+    DNS-lookup:	0.0005 secs, 0.0000 secs, 0.0011 secs
+    req write:	0.0001 secs, 0.0000 secs, 0.0005 secs
+    resp wait:	2.4031 secs, 2.4001 secs, 2.4071 secs
+    resp read:	0.0017 secs, 0.0000 secs, 0.0034 secs
+
+  Status code distribution:
+    [200]	32 responses
+
