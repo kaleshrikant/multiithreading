306049e355218c9100824dab79e3ddee79dd5859
Shrikant Kale <kaleshrikant@live.com>
2025-07-20 13:14:49 +0530

    creation of 1000 virtual threads

--- J2SE-Virtual-Threads/virtual-threads/src/main/java/com/kaleshrikant/VirtualThreadDemo.java
+++ J2SE-Virtual-Threads/virtual-threads/src/main/java/com/kaleshrikant/VirtualThreadDemo.java
@@ -1,7 +1,10 @@
 package com.kaleshrikant;
 
+import java.util.stream.IntStream;
+
 public class VirtualThreadDemo {
-	public static void main ( String[] args ) {
+	public static void main ( String[] args ) throws InterruptedException {
+/*
 
 // Normal Thread
 		Thread
@@ -21,5 +24,21 @@ public class VirtualThreadDemo {
 								System.out.println("Virtual Thread : "+Thread.currentThread())
 						// Virtual Thread : Virtual Thread[#21]/runnable@ForkJoinPool -1-worker-1
 				);
+*/
+		var start = System.currentTimeMillis();
+		var totalThread = 10000;
+		var threads = IntStream.range(0, totalThread)
+				.mapToObj(
+						thCount -> Thread.ofVirtual().unstarted(() -> {
+						})).toList();
+		threads.forEach(Thread::start);
+				// VirtualThread[#20]/runnable@ForkJoinPool-1-worker-1 // VirtualThread[#20]/runnable@ForkJoinPool-1-worker-2
+			// millis used to launch 10000vthreads:58ms
+		for (Thread thread : threads) {
+			thread.join();
+		}
+		var end = System.currentTimeMillis();
+		System.out.println("millis used to launch " + totalThread + "vthreads:" + (end - start) + "ms");
+
 	}
 }
